24e019902daff8570e3907c50da816a9ef5be330
elara04 <60409716+elara04@users.noreply.github.com>
2022-03-19 13:43:18 -0700

    Add files via upload

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,47 +1,44 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 
+
 export default function HeaderTabs() {
-    const [activeTab, setActiveTab] = useState("Delivery");
+    const [activeTab,setActiveTab] = useState("Home");
   return (
     <View style={{flexDirection: "row", alignSelf: "center"}}>
-        <HeaderButton
-            text="Delivery" 
-            btnColor="black" 
-            txtColor="white" 
-            activeTab={activeTab} 
-            setActiveTab={setActiveTab}
-        />
-
-        <HeaderButton
-            text="Pick Up" 
-            btnColor="white" 
-            txtColor="black" 
-            activeTab={activeTab} 
-            setActiveTab={setActiveTab}
-        />
+         <HeaderButton 
+         text="Home" 
+         btnColor="black"
+         textColor="white" 
+         activeTab={activeTab}
+         setActiveTab={setActiveTab}
+         />
+         <HeaderButton 
+         text="Map" 
+         btnColor="white"
+         textColor="black" 
+         activeTab={activeTab}
+         setActiveTab={setActiveTab}
+         />
     </View>
-  )
+  );
 }
 
 const HeaderButton = (props) => (
-    <TouchableOpacity 
+
+    <TouchableOpacity
         style={{
-            backgroundColor: props.activeTab == props.text ?"black" : "white",
-            paddingVertical: 6,
-            paddingHorizontal: 16,
-            borderRadius: 30,
+        backgroundColor: props.activeTab == props.text ? "black" : "white",
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30,
         }}
         onPress={() => props.setActiveTab(props.text)}
-    >
-        <Text 
-            style={{
-                color: props.activeTab == props.text ?"white" : "black",
-                fontSize: 15,
-                fontWeight: "bold",
-            }}
-        >
-            {props.text}
+     >
+        
+        <Text style={{color:props.activeTab == props.text ? "white" : "black",fontSize: 15, fontWeight: "bold"}}>
+             {props.text}
         </Text>
     </TouchableOpacity>
+
 );
--- components/SearchBar.js
+++ components/SearchBar.js
@@ -4,10 +4,16 @@ import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplet
 import Ionicons from "react-native-vector-icons/Ionicons"
 import AntDesign from "react-native-vector-icons/AntDesign"
 
-export default function SearchBar() {
+export default function SearchBar({cityHandler}) {
   return (
     <View style={{marginTop: 15, flexDirection: "row"}}>
       <GooglePlacesAutocomplete
+        query={{key: "AIzaSyBxTeV5YbXOqvO2W7B2-9vgOCJ2hToDfGg"}}
+        onPress={( data, details = null) => {
+            console.log(data.description);
+            const city = data.description.split(",")[0];
+            cityHandler(city);
+        }}
         placeholder="Search"
         styles={{
           textInput: {
@@ -40,7 +46,7 @@ export default function SearchBar() {
                 backgroundColor: "white",
                 padding: 9,
                 borderRadius: 30,
-                alignItems: "center",              
+                alignItems: "center",
 
               }
             }
@@ -53,7 +59,7 @@ export default function SearchBar() {
             <Text>Search</Text>
           </View>
         )}
-      />   
+      />
     </View>
   )
 }
