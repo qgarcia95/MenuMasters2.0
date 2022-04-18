ee61e17a4250dcabc70279310e8eb97ab91aacd9
javithegreat35 <84287901+javithegreat35@users.noreply.github.com>
2022-04-17 20:51:31 -0700

    Update HeaderTabs.js
    Changes to header tab from demo 3. Added functionality to pickup and delivery. Jeffrey assisted with the headertab as well.

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -2,24 +2,38 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 
 
-export default function HeaderTabs() {
-    const [activeTab,setActiveTab] = useState("Home");
+export default function HeaderTabs(props) {
+    // const [activeTab,setActiveTab] = useState("Delivery");
   return (
     <View style={{flexDirection: "row", alignSelf: "center"}}>
          <HeaderButton 
-         text="Home" 
+         text="Delivery" 
          btnColor="black"
          textColor="white" 
-         activeTab={activeTab}
-         setActiveTab={setActiveTab}
+         activeTab={props.activeTab}
+         setActiveTab={props.setActiveTab}
+         />
+         <HeaderButton 
+         text="Pickup" 
+         btnColor="white"
+         textColor="black" 
+         activeTab={props.activeTab}
+         setActiveTab={props.setActiveTab}
+         />
+         <HeaderButton 
+         text="Reviews" 
+         btnColor="black"
+         textColor="white" 
+         activeTab={props.activeTab}
+         setActiveTab={props.setActiveTab}
          />
          <HeaderButton 
          text="Map" 
-         btnColor="white"
-         textColor="black" 
-         activeTab={activeTab}
-         setActiveTab={setActiveTab}
-         />
+         btnColor="black"
+         textColor="white" 
+         activeTab={props.activeTab}
+         setActiveTab={props.setActiveTab}
+         /> 
     </View>
   );
 }
@@ -35,7 +49,7 @@ const HeaderButton = (props) => (
         }}
         onPress={() => props.setActiveTab(props.text)}
      >
-        
+
         <Text style={{color:props.activeTab == props.text ? "white" : "black",fontSize: 15, fontWeight: "bold"}}>
              {props.text}
         </Text>
