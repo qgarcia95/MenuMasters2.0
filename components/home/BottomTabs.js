e57dd09dcfe70c0a5891ccfe4c75cceb3c2569ad
Ryan Luong <ryanluong83@gmail.com>
2022-05-01 20:34:30 -0700

    Add files via upload

--- components/home/BottomTabs.js
+++ components/home/BottomTabs.js
@@ -1,40 +1,44 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5"
 
 export default function BottomTabs() {
   return (
     <View 
-        style={{
-            flexDirection: "row", 
-            margin:10, 
-            marginHorizontal: 30, 
-            justifyContent: "space-between",
+    style={{
+        flexDirection: "row",
+        margin: 10,
+        marginHorizontal:30,
+        justifyContent: "space-between"
         }}
     >
+       {/* <Icon icon="home" text="Home" />
+       <Icon icon="search" text="Browse" /> */}
         <Icon icon="home" text="Home"/>
         <Icon icon="search" text="Browse"/>
-        <Icon icon="heart" text="Favorites"/>
         <Icon icon="map-marked-alt" text="Map"/>
         <Icon icon="shopping-bag" text="Grocery"/>
         <Icon icon="receipt" text="Orders"/>
         <Icon icon="user" text="Account"/>
+       
+      
+      
     </View>
-  )
+  );
 }
 
-const Icon = (props) => (
-    <TouchableOpacity>
-        <View>
-            <FontAwesome5 
-                name={props.icon} 
-                size={25} 
-                style={{
-                    marginBottom: 3,
-                    alignSelf: "center",
-                }} 
-            />
-            <Text>{props.text}</Text>
-        </View>
-    </TouchableOpacity>
-)
+const Icon =(props) => (
+<TouchableOpacity>
+    <View >
+      <FontAwesome5 
+        name={props.icon} 
+        size={25} 
+        style={{
+            marginBottom: 3,
+            alignSelf: "center",
+         }}
+      />
+      <Text>{props.text}</Text>
+     </View>
+</TouchableOpacity>
+);
