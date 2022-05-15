a3d2584d084f80249f8d5dcf4cd890b9308646d2
Quauhtli Garcia-Brindis <79949335+qgarcia95@users.noreply.github.com>
2022-05-14 21:22:23 -0700

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native'
-import Home from "./screens/Home"
-import RestaurantDetail from "./screens/RestaurantDetail"
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import RestaurantDetail from "./screens/RestaurantDetail";
+import {Provider as ReduxProvider} from "react-redux";
+import store from "./redux/store";
+
+
+
 export default function RootNavigation() {
-    const Stack = createStackNavigator()
+    const Stack = createStackNavigator();
+
     const screenOptions = {
         headerShown: false,
-    }
-    return  (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+
+    };
+
+    return (
+    <ReduxProvider store={store}>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="RestaurantDetail" component={RestaurantDetail}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+    </ReduxProvider>
+    );
     
 }
