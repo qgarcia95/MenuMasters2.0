2257384e50a394d0b538f524ee92a8d80fdc20b5
Quauhtli Garcia-Brindis <79949335+qgarcia95@users.noreply.github.com>
2022-05-14 21:17:46 -0700

    Firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYeLMl8PTste_pyy6S0jSgJFgGLWSAX7E",  
+    authDomain: "react-native-menumasters.firebaseapp.com",  
+    projectId: "react-native-menumasters",  
+    storageBucket: "react-native-menumasters.appspot.com",  
+    messagingSenderId: "623218870012",  
+    appId: "1:623218870012:web:336a4f2b4c7ea402f83979",  
+    measurementId: "G-42XFXTX0MY"
+};
